551036f0385bb302ac2b2182f118ffa041fb5b7a
Jackson Oppenheim <jacksonopp@gmail.com>
2019-12-17 14:12:28 -0600

    added mobile responsiveness

--- assets/js/interactiveName.js
+++ assets/js/interactiveName.js
@@ -1,8 +1,8 @@
 let userName = "no name provided"
 document.getElementById("name-submit").addEventListener("click", e => {
   e.preventDefault()
-  userName = document.getElementById("user-name").value || "no name provided";
-  const userNameForDiv = userName === "no name provided" ? "there" : userName
+  userName = document.getElementById("user-name").value || "no name provided"; // set username for tracking
+  const userNameForDiv = userName === "no name provided" ? "there" : userName //set username for div
   const hiNameH1 = document.getElementById("hi-name")
   hiNameH1.innerText = `Hi ${userNameForDiv}!`
   console.log(userName)
